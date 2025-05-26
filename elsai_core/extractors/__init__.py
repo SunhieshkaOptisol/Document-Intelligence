468deb5b916b6aac43c83d975e677734d637ced0
SunhieshkaOptisol <sunhieshka.c@optisolbusiness.com>
2025-05-26 20:45:57 +0530

    vbnm

--- elsai_core/extractors/__init__.py
+++ elsai_core/extractors/__init__.py
@@ -1,4 +1,4 @@
-from .aws_textract import AwsTextractConnector
+
 from .llama_parse_extractor import LlamaParseExtractor
 from .unstructured_excel_loader_service import UnstructuredExcelLoaderService
 from .azure_cognitive_service import AzureCognitiveService
@@ -9,7 +9,7 @@ from .pypdfloader_service import PyPDFTextExtractor
 from .visionai_pdf_extractor import VisionAIExtractor
 
 __all__ = [
-    AwsTextractConnector,
+    
     LlamaParseExtractor,
     UnstructuredExcelLoaderService,
     AzureCognitiveService,
